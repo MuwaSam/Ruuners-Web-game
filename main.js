223351a8f671f66ccb06cc294d9462fe42552656
mugerwasamuel8@gmail.com <mugerwasamuel8@gmail.com>
2023-12-17 10:12:19 +0300

    update main.js

--- main.js
+++ main.js
@@ -2,8 +2,11 @@ var player = document.getElementById("player");
 
 
 function jump(){
-player.style.animation = "jumped 2s infinite";
 
+player.style.animation = "jumped 2s";
+  setTimeout(function(){
+player.style.animation = "";
+},1000);
 }
 alert("hello");
 console.log("hlli");
