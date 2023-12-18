7869793e6b6924650ce4e3d33085d79a4ad44c04
mugerwasamuel8@gmail.com <mugerwasamuel8@gmail.com>
2023-12-18 15:33:58 +0300

    bugy collusion establishment

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
 var player = document.getElementById("player");
+var block = document. getElementById("object");
+var game_state= true;
 
-
-function jump(){
-
+function jump(block_position){
+  var player_position=parseInt(window.getComputedStyle(player).getPropertyValue("top"));
+  
+  var block_position=parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+console.log(player_position);
 player.style.animation = "jumped 2s";
   setTimeout(function(){
 player.style.animation = "";
@@ -10,3 +14,15 @@ player.style.animation = "";
 }
 alert("hello");
 console.log("hlli");
+
+var game_over= setInterval(function(){
+  var block_position = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+
+var player_position=parseInt(window.getComputedStyle(player).getPropertyValue("top"));
+
+if (player_position<=100 && block_position<=80){
+console.log("game over");
+}
+
+
+},10)
